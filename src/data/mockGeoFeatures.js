7a53d0e0449b631a36c52bb281bcf53f6e4c4af5
thetzin <thetzin@innoix-solutions.com>
2025-06-07 11:33:21 +0630

    File and Folder Architecture

---
+++ src/data/mockGeoFeatures.js
@@ -0,0 +1,95 @@
+export const mockGeoFeatures = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      id: "MM",
+      properties: { name: "Myanmar" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[96, 21], [97, 21], [97, 22], [96, 22], [96, 21]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "TH",
+      properties: { name: "Thailand" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[100, 15], [101, 15], [101, 16], [100, 16], [100, 15]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "IN",
+      properties: { name: "India" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[77, 28], [78, 28], [78, 29], [77, 29], [77, 28]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "CN",
+      properties: { name: "China" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[110, 35], [111, 35], [111, 36], [110, 36], [110, 35]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "JP",
+      properties: { name: "Japan" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[138, 36], [139, 36], [139, 37], [138, 37], [138, 36]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "KR",
+      properties: { name: "South Korea" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[127, 37], [128, 37], [128, 38], [127, 38], [127, 37]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "VN",
+      properties: { name: "Vietnam" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[106, 16], [107, 16], [107, 17], [106, 17], [106, 16]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "BD",
+      properties: { name: "Bangladesh" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[90, 23], [91, 23], [91, 24], [90, 24], [90, 23]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "LA",
+      properties: { name: "Laos" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[102, 18], [103, 18], [103, 19], [102, 19], [102, 18]]]
+      }
+    },
+    {
+      type: "Feature",
+      id: "KH",
+      properties: { name: "Cambodia" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [[[104, 12], [105, 12], [105, 13], [104, 13], [104, 12]]]
+      }
+    }
+  ]
+};
